903a9d07b1c15500ec6c31a307b84f3cfcf7dbf9
Michael Delle <dravenlon@gmail.com>
2014-09-28 20:47:15 -0400

    WebSupport fix

--- templates/webSupport_nav.php
+++ templates/webSupport_nav.php
@@ -1,6 +1,6 @@
 <?php /* templates/webSupport_nav.php */
 
-$websupport_nav.='<ul>'.
+$websupport_nav='<ul>'.
 	'<li'.Document::addHereClass(APPLICATION_URL.'webSupport/FAQ.php').'>'.
 		'<a href="'.APPLICATION_URL.'webSupport/FAQ.php"'.Document::addHereClass(APPLICATION_URL.'webSupport/FAQ.php').' title="Frequently Asked Questions"><abbr title="Frequently Asked Questions">FAQ</abbr></a>'.
 	'</li>'.
