c92ee41d7e06d4b099d365ca01c7259bd01d0dfc
Jon Ryser <bigtalk@jonryser.com>
2014-11-02 00:48:12 -0700

    Added Ant Minifier Scripts

---
+++ dev/minifier/setup.bat
@@ -0,0 +1,3 @@
+set ANT_HOME=C:\ant
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_07
+set PATH=%PATH%;%ANT_HOME%\bin
---
+++ dev/minifier/setup.sh
@@ -0,0 +1,3 @@
+export ANT_HOME=/ant
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+export PATH=${PATH}:${ANT_HOME}/bin
