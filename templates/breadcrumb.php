a1279425af59aea91d55ecf207e3454ddb88cb37
Jon Ryser <contact@jonryser.com>
2014-11-23 22:58:07 -0800

    Added breadcrumb template

---
+++ templates/breadcrumb.php
@@ -0,0 +1,8 @@
+<?php /* templates/breadcrumb.php */
+
+# Display the breadcrumb.
+echo '<section id="breadcrumb" class="breadcrumb">',
+	'<a class="breadcrumb-1" href="',APPLICATION_URL,'">Home</a>',
+	'<span class="crumbSeparator"></span>',
+	$main_content->getPageTitle(),
+'</section>';
