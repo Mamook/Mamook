fec7e3cd3eaa833d7c31bceb717f49b3ca9785f2
bigtalk <contact@jonryser.com>
2015-05-02 11:32:48 -0700

    Added Category form files (beginning)

---
+++ framework/application/templates/forms/category_form.php
@@ -0,0 +1,87 @@
+<?php /* framework/application/templates/forms/category_form.php */
+
+require Utility::locateFile(TEMPLATES.'forms'.DS.'category_form_defaults.php');
+$display_delete_form=$fp->processCategory($default_data);
+
+# Set the CategoryFormPopulator object from the CategoryFormProcessor data member to a variable.
+$populator=$fp->getPopulator();
+# Set the Category object from the CategoryFormPopulator data member to a variable.
+$category=$populator->getCategoryObject();
+
+$select=TRUE;
+
+$descriptive='category';
+if(isset($is_playlist_form) && ($is_playlist_form===TRUE))
+{
+	$descriptive='playlist';
+}
+
+$duplicates=$fp->getDuplicates();
+if(empty($duplicates))
+{
+	# Set the default sub title of the page to "Add New Category".
+	$sub_title='Add New '.ucfirst($descriptive);
+	# Set the category's name to a local variable.
+	$category_name=$category->getCategory();
+	# Check if this is an edit or delete page.
+	if(isset($_GET['category']))
+	{
+		# Set the page's subtitle as an edit page.
+		$sub_title='Edit <span>'.$category_name.'</span>';
+		# Check if this is a delete page.
+		if(isset($_GET['delete']))
+		{
+			# Set the page's subtitle as a delete page.
+			$sub_title='Delete <span>'.$category_name.'</span>';
+		}
+	}
+	# Set the sub title.
+	$main_content->setSubTitle($sub_title);
+
+	$display.='<div id="category_form" class="form">';
+
+	# create and display form.
+	$display.=$head;
+
+	# Add the statement about requirements.
+	$display.='<span class="required">* = required field</span>';
+
+	# Instantiate a new FormGenerator object.
+	$fg=new FormGenerator('category', $fp->getFormAction(), 'POST', '_top', TRUE);
+	$fg->addElement('hidden', array('name'=>'_submit_check', 'value'=>'1'));
+	$fg->addElement('hidden', array('name'=>'_unique', 'value'=>(string)$populator->getUnique()));
+	$fg->addFormPart('<fieldset>');
+	$fg->addFormPart('<ul>');
+	$fg->addFormPart('<li>');
+	$fg->addFormPart('<label class="label" for="name"><span class="required">*</span> Name</label>');
+	$fg->addElement('text', array('name'=>'name', 'id'=>'name', 'value'=>$category->getCategory()));
+	$fg->addFormPart('</li>');
+	$fg->addFormPart('<li>');
+	$button_value='Submit';
+	# Check if this is an edit page.
+	if(isset($_GET['category']) OR isset($_GET['playlist']))
+	{
+		$button_value='Update';
+	}
+	$fg->addElement('submit', array('name'=>'category', 'value'=>$button_value), '', NULL, 'submit-post');
+	# Check if this is an edit page.
+	if(isset($_GET['category']) && !isset($_GET['delete']))
+	{
+		$fg->addFormPart('<a href="'.ADMIN_URL.'ManageContent/categories/?'.$descriptive.'='.$category->getID().'&amp;delete" class="submit-delete" title="Delete This">Delete</a>');
+	}
+	$fg->addElement('submit', array('name'=>'category', 'value'=>'Reset'), '', NULL, 'submit-reset');
+	if(isset($_GET['add']))
+	{
+		$fg->addElement('submit', array('name'=>'category', 'value'=>'Go Back'), '', NULL, 'submit-back');
+	}
+	$fg->addFormPart('</li>');
+	$fg->addFormPart('</ul>');
+	$fg->addFormPart('</fieldset>');
+	$display.=$fg->display();
+	$display.='</div>';
+
+	# Create a new Category object.
+	$category=New Category();
+	$display.=$category->displayCategoryList();
+}
+$display=$display_delete_form.$display;
---
+++ framework/application/templates/forms/category_form_defaults.php
@@ -0,0 +1,34 @@
+<?php /* framework/application/templates/forms/category_form_defaults.php */
+
+# Create defaults.
+$category_id=NULL;
+$category_name=NULL;
+$category_unique=0; # Set the default to "Not Unique" (0)
+
+# Check if there is GET data called "category".
+if(isset($_GET['category']))
+{
+	# Get the Category class.
+	require_once Utility::locateFile(MODULES.'Content'.DS.'Category.php');
+	# Instantiate a new instance of the Category class.
+	$category=new Category();
+	# Set the passed category ID to the Category data member, effectively "cleaning" it.
+	$category->setID($_GET['category']);
+	# Set the cleaned SubContent id to a local variable.
+	$inst_id=$category->getID();
+	# Get the category's content from the `category` table.
+	if($category->getThisCategory($inst_id)===TRUE)
+	{
+		# Reset the defaults.
+		$category_id=$inst_id;
+		$category_name=$category->getCategory();
+		$category_unique=1; # Set to "Unique" (1) since it is already an category.
+	}
+}
+
+# The key MUST be the name of a "set" mutator method in either the Category, CategoryFormPopulator, or FormPopulator classes (ie setID, setUnique).
+$default_data=array(
+		'ID'=>$category_id,
+		'Category'=>$category_name,
+		'Unique'=>$category_unique
+	);
