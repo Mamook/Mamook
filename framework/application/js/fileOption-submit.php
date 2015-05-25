b4d4487a12362380e91c09ca5228597ddcc472cc
bigtalk <contact@jonryser.com>
2015-05-25 12:14:21 -0700

    Cleaned up the fileOption-submit JS and minified it.

--- framework/application/js/fileOption-submit.php
+++ framework/application/js/fileOption-submit.php
@@ -1,52 +1,30 @@
 <?php /* application/js/fileOption-submit.php */
 
-# Minified version. (minified at http://closure-compiler.appspot.com/home)
-$js='var audio=$(".submit-audio"),content=$(".submit-content"),file=$(".submit-file"),post=$(".submit-post"),product=$(".submit-product"),video=$(".submit-video"),submitButton;submitButton=0<audio.length?audio:0<content.length?content:0<file.length?file:0<post.length?post:0<product.length?product:video;$("#fileOption").change(function(){$(submitButton).click()});$("#imageOption").change(function(){$(submitButton).click()});$("#institution").change(function(){"add"==$(this).val()&&$(submitButton).click()});
-$("#language").change(function(){"add"==$(this).val()&&$(submitButton).click()});$("#text_language").change(function(){"add"==$(this).val()&&$(submitButton).click()});$("#trans_language").change(function(){"add"==$(this).val()&&$(submitButton).click()});$("#publisher").change(function(){"add"==$(this).val()&&$(submitButton).click()});';
+# Minified version.
+$js='$(function(){for(var c=$("[class|=submit]"),b=[$("#fileOption"),$("#imageOption"),$("#institution"),$("#language"),$("#text_language"),$("#trans_language"),$("#publisher")],a=0;a<b.length;++a)b[a].change(function(){"add"==$(this).val()&&c.click()})});';
 
 # Long version.
 /*$js='
-var audio = $(\'.submit-audio\');
-var content = $(\'.submit-content\');
-var file = $(\'.submit-file\');
-var post = $(\'.submit-post\');
-var product = $(\'.submit-product\');
-var video = $(\'.submit-video\');
-var submitButton;
-if(audio.length>0)
-   submitButton=audio;
-else if(content.length>0)
-   submitButton=content;
-else if(file.length>0)
-   submitButton=file;
-else if(post.length>0)
-   submitButton=post;
-else if(product.length>0)
-   submitButton=product;
-else submitButton=video;
-$(\'#fileOption\').change(function(){
-   $(submitButton).click();
-});
-$(\'#imageOption\').change(function(){
-   $(submitButton).click();
-});
-$(\'#institution\').change(function(){
-   if($(this).val()==\'add\')
-       $(submitButton).click();
-});
-$(\'#language\').change(function(){
-   if($(this).val()==\'add\')
-       $(submitButton).click();
-});
-$(\'#text_language\').change(function(){
-   if($(this).val()==\'add\')
-       $(submitButton).click();
-});
-$(\'#trans_language\').change(function(){
-   if($(this).val()==\'add\')
-       $(submitButton).click();
-});
-$(\'#publisher\').change(function(){
-   if($(this).val()==\'add\')
-       $(submitButton).click();
-});';*/
+	// Wrap it all in the jQuery(document).ready method to ensure the internal variables and functions are not invasive to the page and so that this will execute after the document is loaded.
+	$(function(){
+		// Create a local variable to hold the submit button on the page.
+		var submitButton = $("[class|=submit]");
+		// Pass each element as an element of the elementArray to the "clickOnChange" method.
+		clickOnChange([$("#fileOption"), $("#imageOption"), $("#institution"), $("#language"), $("#text_language"), $("#trans_language"), $("#publisher")]);
+
+		// The clickOnChange method accepts an Array of jQuery Objects.
+		function clickOnChange(elementArray){
+			// Loop through the jQuery Objects.
+			for(var key=0; key<elementArray.length; ++key)
+			{
+				// Set the current jQuery Object to a local variable.
+				var $element=elementArray[key];
+				// Set a funtion to the onChange event for this element.
+				$element.change(function(){
+					// Check if the value of the element on change was "add". If so, trigger the click of the submit button.
+					if($(this).val()=="add")
+						submitButton.click();
+				});
+			}
+		};
+	});';*/
