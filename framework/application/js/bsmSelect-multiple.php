ca816428e41e1967538052f55afd1cfe6efe4817
bigtalk <contact@jonryser.com>
2015-05-25 11:00:31 -0700

    Cleaned up and minified bsmSelect-multiple JS.

--- framework/application/js/bsmSelect-multiple.php
+++ framework/application/js/bsmSelect-multiple.php
@@ -1,19 +1,21 @@
 <?php /* application/js/bsmSelect-multiple.php */
+$js='$("select[multiple]").bsmSelect({addItemTarget:"original",hideWhenAdded:!1,removeLabel:"X",animate:!0,plugins:[$.bsmSelect.plugins.compatibility()],containerClass:"bsm",selectClass:"select",listClass:"bsm-list",listItemClass:"bsm-item",listItemLabelClass:"bsm-label",removeClass:"remove"});$(".remove").attr({title:"Remove This"});$("select[multiple]").change(function(){$(".remove").attr({title:"Remove This"})});';
 
-$js='$(\'select[multiple]\').bsmSelect({'.
-		'addItemTarget: \'original\','."\n".
-		'hideWhenAdded: false,'."\n".
-		'removeLabel: \'X\','."\n".
-		'animate: true,'."\n".
-		'plugins: [ $.bsmSelect.plugins.compatibility() ],'."\n".
-		'containerClass: \'bsm\','."\n".						// Class for container that wraps this widget
-		'selectClass: \'select\','."\n".						// Class for the <select>
-		'listClass: \'bsm-list\','."\n".						// Class for the list ($ol)
-		'listItemClass: \'bsm-item\','."\n".				// Class for the <li> list items
-		'listItemLabelClass: \'bsm-label\','."\n".	// Class for the label text that appears in list items
-		'removeClass: \'remove\''.									// Class given to the "remove" link
+# Big
+/* $js='$("select[multiple]").bsmSelect({'.
+		'addItemTarget:"original",'.
+		'hideWhenAdded:false,'.
+		'removeLabel:"X",'.
+		'animate:true,'.
+		'plugins:[$.bsmSelect.plugins.compatibility()],'.
+		'containerClass:"bsm",'.						// Class for container that wraps this widget
+		'selectClass:"select",'.						// Class for the <select>
+		'listClass:"bsm-list",'.						// Class for the list ($ol)
+		'listItemClass:"bsm-item",'.				// Class for the <li> list items
+		'listItemLabelClass:"bsm-label",'.	// Class for the label text that appears in list items
+		'removeClass:"remove"'.							// Class given to the "remove" link
 	'});'.
-	'$(\'.remove\').attr(\'title\', \'Remove This\');'.
-	'$(\'select[multiple]\').change(function(){'.
-		'$(\'.remove\').attr(\'title\', \'Remove This\')'.
-	'});';
+	'$(".remove").attr({title:"Remove This"});'.
+	'$("select[multiple]").change(function(){'.
+		'$(".remove").attr({title:"Remove This"})'.
+	'});'; */
