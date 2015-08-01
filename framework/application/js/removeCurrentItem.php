5ce39ca530652f2e1aaab6c5f39ff146d5aed251
Jon Ryser <contact@jonryser.com>
2015-08-01 16:05:04 -0700

    Adding missing "removeCurrentItem" JS

---
+++ framework/application/js/removeCurrentItem.php
@@ -0,0 +1,21 @@
+<?php /* application/js/removeCurrentItem.php */
+
+# Minified version.
+$js='$(function(){$(".file-current .remove").click(function(){var a=$(this).closest(".file-current");$(this).prev("input").clone().attr({name:"_cv_remove"}).appendTo(a.closest("li"));a.remove();return!1})});';
+
+# Long version.
+/*$js='// Wrap it all in the jQuery(document).ready method to ensure the internal variables and functions are not invasive to the page and so that this will execute after the document is loaded.
+	$(function(){
+		// Create a local variable to hold the "remove" button. When it is clicked, remove the parent element with the class "file-current".
+		var removeButton = $(".file-current .remove")
+			.click(function(){
+				var currentFileContainer = $(this).closest(".file-current");
+				// Clone the hidden input.
+				var hidden = $(this).prev("input")
+					.clone()
+					.attr({name:"_cv_remove"})
+					.appendTo(currentFileContainer.closest("li"));
+				currentFileContainer.remove();
+				return false;
+			});
+	});';*/
