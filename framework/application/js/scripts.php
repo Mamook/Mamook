e5c325f47ea83d23b63efff2c579107e3837b1f5
Jon Ryser <contact@jonryser.com>
2015-08-30 21:33:36 -0700

    Made js more readable. Added comments. Wrapped in $(window).load

--- framework/application/js/scripts.php
+++ framework/application/js/scripts.php
@@ -3,7 +3,38 @@
 	# If Javascript is enabled in the user's browser, display errors in a java created div.
 	$js=$this->addJSErrorBox();
 
+	# Open "window load" (better than "document ready") to put all JavaScript calls inside of.
 	$js.=
-	'$("#wrapper").removeClass("noscript");$(function(){clearInput("#emailGo","value")});'.
-	'$(function(){$("[rel^='.FW_POPUP_HANDLE.']").fwPopup({opacity:1,theme:"",social_tools:null})});'.
+	'$(window).load(function(){';
+
+	# Remove the "noscript" class. If there is no JavaScript available, this class will remain.
+	$js.=
+	'$("#wrapper").removeClass("noscript");';
+
+	# If the user's device is mobile, add the "mobile" class.
+	$js.=
+	'if(fwIsMobile)$("body").addClass("mobile");';
+
+	# Execute "clearInput".
+	$js.=
+	'clearInput("#emailGo","value");';
+
+	# Instantiate "fwPopup".
+	$js.=
+	'$(function(){$("[rel^='.FW_POPUP_HANDLE.']").fwPopup({opacity:1,theme:"",social_tools:null})});';
+
+	# Add the Google+ button to the AddThis buttons.
+	$js.=
 	'$(".addthis_button_google_plusone").attr("g:plusone:annotation","none");';
+
+	# Instantiate the "fwNav" on the main nav for small screens (mobile).
+	//$js.=
+	//'$(".mainnav").fwNav({navList:".mainnav .nav-1",navHandle:"mainnavHandle",maxContainerWidth:"100%",maxContainerHeight:"300%",minContainerWidth:69,minContainerHeight:43});';
+
+	# Instantiate the "fwNav" on the box2 nav for small screens (mobile)
+	//$js.=
+	//'$(".box2").fwNav({navList:".box2 .nav-1",navHandle:"box2Handle",maxContainerWidth:"100%",maxContainerHeight:"300%",minContainerWidth:47,minContainerHeight:29});';
+
+	# Close "window load".
+	$js.=
+	'});';
