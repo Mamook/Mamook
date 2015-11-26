fbf29a481440e1b3f5f5cd3fd75a1b7e61c03a37
Jon Ryser <contact@jonryser.com>
2015-11-26 12:38:54 -0800

    Fixing image submit on forms

--- framework/application/js/fileOption-submit.php
+++ framework/application/js/fileOption-submit.php
@@ -1,14 +1,14 @@
 <?php /* application/js/fileOption-submit.php */
 
 # Minified version.
-$js='$(function(){for(var c=$("[class|=submit]"),b=[$("#fileOption"),$("#imageOption"),$("#institution"),$("#language"),$("#text_language"),$("#trans_language"),$("#publisher")],a=0;a<b.length;++a)b[a].change(function(){"add"==$(this).val()||"select"==$(this).val()&&c.click()})});';
+$js='$(function(){for(var c=$("[class|=submit]").not("[value=Reset]"),b=[$("#fileOption"),$("#imageOption"),$("#institution"),$("#language"),$("#text_language"),$("#trans_language"),$("#publisher")],a=0;a<b.length;++a)b[a].change(function(){"add"==$(this).val()||"select"==$(this).val()&&c.click()})});';
 
 # Long version.
 /*$js='
 	// Wrap it all in the jQuery(document).ready method to ensure the internal variables and functions are not invasive to the page and so that this will execute after the document is loaded.
 	$(function(){
 		// Create a local variable to hold the submit button on the page.
-		var submitButton = $("[class|=submit]");
+		var submitButton = $("[class|=submit]").not("[value=Reset]");
 		// Pass each element as an element of the elementArray to the "clickOnChange" method.
 		clickOnChange([$("#fileOption"), $("#imageOption"), $("#institution"), $("#language"), $("#text_language"), $("#trans_language"), $("#publisher")]);
 
