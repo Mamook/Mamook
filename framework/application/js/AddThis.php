d32b0e802ce25bc12af510478d7be2abe84cf448
Michael Delle <dravenlon@gmail.com>
2016-01-13 22:59:17 -0500

    Removing Git Test

--- framework/application/js/AddThis.php
+++ framework/application/js/AddThis.php
@@ -1,4 +1,4 @@
-<?php /* framework/application/js/AddThis.php Hi Draven */
+<?php /* framework/application/js/AddThis.php */
 
 $facebook_id=((defined('FB_PAGE_ID')) ? FB_PAGE_ID : '');
 $rss_id=((defined('RSS_ID')) ? RSS_ID : '');
