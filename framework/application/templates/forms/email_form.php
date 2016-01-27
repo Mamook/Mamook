c6cf80b4cb77992e367bf90af9772464779fec15
Michael Delle <dravenlon@gmail.com>
2016-01-27 17:23:33 -0500

    Fixed email form on contributor profiles

--- framework/application/templates/forms/email_form.php
+++ framework/application/templates/forms/email_form.php
@@ -5,7 +5,7 @@ $fp->processEmail($default_data);
 # Set the CertificateAppFormPopulator object to a variable.
 $populator=$fp->getPopulator();
 # Set the Email object created in CertificateAppFormPopulator to a variable.
-$email=$populator->getEmailObject();
+$email_obj=$populator->getEmailObject();
 if(isset($_GET['mail']))
 {
 	$recipients=trim(strip_tags($_GET['mail']));
@@ -13,17 +13,17 @@ if(isset($_GET['mail']))
 	$good_url=APPLICATION_URL.WebUtility::removeIndex(HERE).'?mail='.$recipients.'&success';
 	$bad_url=APPLICATION_URL.WebUtility::removeIndex(HERE).'?mail='.$recipients.'&mail_error';
 }
-elseif(isset($_GET['member']))
+elseif(isset($_GET['member']) || isset($_GET['contributor']))
 {
 	# Get the recipients from the Email class that was instantiated in the Populator.
-	$recipients=$email->getRecipients();
+	$recipients=$email_obj->getRecipients();
 }
 else
 {
 	$recipients=$recipients;
 }
 # Set the email recipient(s). (See data/formmail.ini)
-$email->setRecipients($recipients);
+$email_obj->setRecipients($recipients);
 
 $display.='<div id="email_form" class="form">';
 # Create and display form
@@ -31,7 +31,7 @@ $display.=$head;
 # Instantiate a new FormGenerator object.
 $form_mail=new FormGenerator('email_form', $fp->getFormAction(), 'POST', $fp->getTarget(), $fp->getUpload(), 'form-email');
 $form_mail->addElement('hidden', array('name'=>'_submit_check', 'value'=>'1'));
-$form_mail->addElement('hidden', array('name'=>'recipients', 'value'=>$email->getRecipients()));
+$form_mail->addElement('hidden', array('name'=>'recipients', 'value'=>$email_obj->getRecipients()));
 $form_mail->addElement('hidden', array('name'=>'mail_options', 'value'=>'CharSet=UTF-8,'.((isset($html_template)) ? 'HTMLTemplate='.$html_template : ((isset($plain_template)) ? 'PlainTemplate='.$plain_template : 'PlainTemplate=form_email_template.txt')).',FromAddr='.preg_replace('/@/', '_form_', SMTP_USER).',FromLineStyle=QuotedNameRouteAddr,AlwaysList'));
 $form_mail->addElement('hidden', array('name'=>'good_url', 'value'=>$good_url));
 $form_mail->addElement('hidden', array('name'=>'bad_url', 'value'=>$bad_url));
@@ -44,15 +44,15 @@ $form_mail->addFormPart('<fieldset>');
 $form_mail->addFormPart('<ul>');
 $form_mail->addFormPart('<li>');
 $form_mail->addFormPart('<label class="label" for="realname">Name</label>');
-$form_mail->addElement('text', array('id'=>'realname', 'name'=>'realname', 'value'=>$email->getSenderName()));
+$form_mail->addElement('text', array('id'=>'realname', 'name'=>'realname', 'value'=>$email_obj->getSenderName()));
 $form_mail->addFormPart('</li>');
 $form_mail->addFormPart('<li>');
 $form_mail->addFormPart('<label class="label" for="email">E-mail</label>');
-$form_mail->addElement('text', array('id'=>'email', 'name'=>'email', 'value'=>$email->getSenderEmail()));
+$form_mail->addElement('text', array('id'=>'email', 'name'=>'email', 'value'=>$email_obj->getSenderEmail()));
 $form_mail->addFormPart('</li>');
 $form_mail->addFormPart('<li>');
 $form_mail->addFormPart('<label class="label" for="subject">Subject</label>');
-$form_mail->addElement('text', array('id'=>'subject', 'name'=>'subject', 'value'=>$email->getSubject()));
+$form_mail->addElement('text', array('id'=>'subject', 'name'=>'subject', 'value'=>$email_obj->getSubject()));
 $form_mail->addFormPart('</li>');
 if($fp->getUpload()===TRUE)
 {
@@ -79,7 +79,7 @@ if($fp->getUpload()===TRUE)
 }
 $form_mail->addFormPart('<li>');
 $form_mail->addFormPart('<label class="label msg" for="mesg">Message</label>');
-$form_mail->addElement('textarea', array('id'=>'mesg', 'name'=>'mesg', 'text'=>$email->getMessage()));
+$form_mail->addElement('textarea', array('id'=>'mesg', 'name'=>'mesg', 'text'=>$email_obj->getMessage()));
 $form_mail->addFormPart('</li>');
 $form_mail->addFormPart('<li>');
 $form_mail->addElement('submit', array('name'=>'send', 'value'=>'Send Email'), '', NULL, 'submit-email');
