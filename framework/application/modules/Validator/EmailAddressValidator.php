58e68656ba9add19dd7a31123ab4b92ac2d03d1b
Jon Ryser <contact@jonryser.com>
2016-02-22 12:55:57 -0800

    Moved EmailDdressValidation Class to its own file. Added additional validation to validEmail and ipValid.

---
+++ framework/application/modules/Validator/EmailAddressValidator.php
@@ -0,0 +1,223 @@
+<?php /* Requires PHP 5.4+ */
+
+/**
+ * EmailAddressValidator Class
+ * http://code.google.com/p/php-email-address-validation/
+ *
+ * Released under New BSD license
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * Sample Code
+ * ----------------
+ * $validator = new EmailAddressValidator;
+ * if($validator->check_email_address('test@example.org'))
+ * {
+ * 	# Email address is technically valid
+ * }
+ */
+
+class EmailAddressValidator
+{
+	/**
+	 * check_email_address
+	 *
+	 * Check email address validity
+	 *
+	 * @param	strEmailAddress	Email address to be checked
+	 * @param	ping						Should it ping the domain name to see if it is an active domain?
+	 * @return								True if email is valid, FALSE if not
+	 * @access								public
+	 */
+	public function check_email_address($strEmailAddress, $ping=FALSE)
+	{
+		# Control characters are not allowed
+		if(preg_match('/[\x00-\x1F\x7F-\xFF]/', $strEmailAddress))
+		{
+			return FALSE;
+		}
+
+		# Check email length - min 3 (a@a), max 256
+		if(!$this->check_text_length($strEmailAddress, 3, 256))
+		{
+			return FALSE;
+		}
+
+		# Split it into sections using last instance of "@"
+		$intAtSymbol = strrpos($strEmailAddress, '@');
+		if($intAtSymbol===FALSE)
+		{
+			# No "@" symbol in email.
+			return FALSE;
+		}
+		$arrEmailAddress[0] = substr($strEmailAddress, 0, $intAtSymbol);
+		$arrEmailAddress[1] = substr($strEmailAddress, $intAtSymbol + 1);
+
+		/*
+		 Count the "@" symbols. Only one is allowed, except where
+		 contained in quote marks in the local part. Quickest way to
+		 check this is to remove anything in quotes. We also remove
+		 characters escaped with backslash, and the backslash
+		 character.
+		 */
+		$arrTempAddress[0] = preg_replace('/\./'
+			,''
+			,$arrEmailAddress[0]);
+		$arrTempAddress[0] = preg_replace('/"[^"]+"/'
+			,''
+			,$arrTempAddress[0]);
+		$arrTempAddress[1] = $arrEmailAddress[1];
+		$strTempAddress = $arrTempAddress[0] . $arrTempAddress[1];
+
+		# Then check - should be no "@" symbols.
+		if(strrpos($strTempAddress, '@')!==FALSE)
+		{
+			# "@" symbol found
+			return FALSE;
+		}
+
+		# Check local portion
+		if(!$this->check_local_portion($arrEmailAddress[0]))
+		{
+			return FALSE;
+		}
+
+		# Check domain portion
+		if(!$this->check_domain_portion($arrEmailAddress[1], $ping))
+		{
+			return FALSE;
+		}
+
+		# If we're still here, all checks above passed. Email is valid.
+		return TRUE;
+	}
+
+	/**
+	 * check_local_portion
+	 *
+	 * Checks email section before "@" symbol for validity
+	 *
+	 * @param	strLocalPortion     Text to be checked
+	 * @return	True if local portion is valid, FALSE if not
+	 * @access	protected
+	 */
+	protected function check_local_portion($strLocalPortion)
+	{
+		# Local portion can only be from 1 to 64 characters, inclusive.
+		# Please note that servers are encouraged to accept longer local
+		# parts than 64 characters.
+		if(!$this->check_text_length($strLocalPortion, 1, 64))
+		{
+			return FALSE;
+		}
+		# Local portion must be:
+		# 1) a dot-atom (strings separated by periods)
+		# 2) a quoted string
+		# 3) an obsolete format string (combination of the above)
+		$arrLocalPortion=explode('.', $strLocalPortion);
+		for($i=0, $max=sizeof($arrLocalPortion); $i<$max; $i++)
+		{
+			if(!preg_match('.^('.
+				'([A-Za-z0-9!#$%&\'*+/=?^_`{|}~-]'.
+				'[A-Za-z0-9!#$%&\'*+/=?^_`{|}~-]{0,63})'.
+				'|'.
+				'("[^\\\"]{0,62}")'.
+				')$.',
+				$arrLocalPortion[$i]))
+			{
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+	/**
+	 * check_domain_portion
+	 *
+	 * Checks email section after "@" symbol for validity
+	 *
+	 * @param	strDomainPortion     Text to be checked
+	 * @return	True if domain portion is valid, FALSE if not
+	 * @access	protected
+	 */
+	protected function check_domain_portion($strDomainPortion, $ping=FALSE)
+	{
+		# Total domain can only be from 1 to 255 characters, inclusive
+		if(!$this->check_text_length($strDomainPortion, 1, 255))
+		{
+			return FALSE;
+		}
+		# Check if domain is IP, possibly enclosed in square brackets.
+		if(preg_match('/^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])'
+			.'(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$/'
+			,$strDomainPortion) ||
+			preg_match('/^\[(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])'
+			.'(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}\]$/'
+			,$strDomainPortion))
+		{
+			return TRUE;
+		}
+		else
+		{
+			$arrDomainPortion=explode('.', $strDomainPortion);
+			if(sizeof($arrDomainPortion)<2)
+			{
+				return FALSE; # Not enough parts to domain
+			}
+			for($i=0, $max=sizeof($arrDomainPortion); $i<$max; $i++)
+			{
+				# Each portion must be between 1 and 63 characters, inclusive
+				if(!$this->check_text_length($arrDomainPortion[$i], 1, 63))
+				{
+					return FALSE;
+				}
+				if (!preg_match('/^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|'
+					.'([A-Za-z0-9]+))$/', $arrDomainPortion[$i]))
+				{
+					return FALSE;
+				}
+				if($i==$max-1)
+				{ # TLD cannot be only numbers
+					if(strlen(preg_replace('/[0-9]/', '', $arrDomainPortion[$i])) <= 0)
+					{
+						return FALSE;
+					}
+				}
+			}
+		}
+		if($ping!==FALSE)
+		{
+			# Check if the email host is a valid host.
+			if(checkdnsrr($strDomainPortion)===FALSE)
+			{
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+	/**
+	 * check_text_length
+	 *
+	 * Check given text length is between defined bounds
+	 *
+	 * @param	strText     Text to be checked
+	 * @param	intMinimum  Minimum acceptable length
+	 * @param	intMaximum  Maximum acceptable length
+	 * @return	True if string is within bounds (inclusive), FALSE if not
+	 * @access	protected
+	 */
+	protected function check_text_length($strText, $intMinimum, $intMaximum)
+	{
+		# Minimum and maximum are both inclusive
+		$intTextLength = strlen($strText);
+		if(($intTextLength<$intMinimum) || ($intTextLength>$intMaximum))
+		{
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+	}
+
+} # End EmailAddressValidator
--- framework/application/modules/Validator/Validator.php
+++ framework/application/modules/Validator/Validator.php
@@ -180,17 +180,29 @@ class Validator
 	/**
 	 * validEmail
 	 *
-	 * Determines if the passed param is a valid email.
+	 * Determines if the passed email address is valid.
 	 * Returns FALSE if the email address is not valid.
 	 *
 	 * @param	$email (The email to check.)
+	 * @param	$ping (Should it ping the domain name to see if it is an active domain?)
 	 * @access	public
 	 */
 	public function validEmail($email, $ping=FALSE)
 	{
+		# Set valid to FALSE by default.
+		$valid=FALSE;
+		# Clean up the passed email address.
 		$email=trim($email);
-		$validate=new EmailAddressValidator();
-		$valid=$validate->check_email_address($email, $ping);
+
+		# Check if the passed email address is NOT empty. It should be at least 5 characters and no more than 256 characters.
+		$empty_email=$this->isEmpty($email, 5, 256);
+		if($empty_email===FALSE)
+		{
+			# Get the EmailAddressValidator class and instantiate a new instance.
+			require_once Utility::locateFile(MODULES.'Validator'.DS.'EmailAddressValidator.php');
+			$validate=new EmailAddressValidator();
+			$valid=$validate->check_email_address($email, $ping);
+		}
 		return $valid;
 	} #==== End -- validEmail
 
@@ -297,254 +309,33 @@ class Validator
 	 * A wrapper method for ipValid() from the IP calss.
 	 *
 	 * Will determine if a given IP address is valid or not.
-	 * Will set the version of the IP address to the $ip_version data member.
 	 *
 	 * @access	public
-	 * @param	$ip						The IP address to validate
+	 * @param		$ip						The IP address to validate
 	 * @return	boolean
 	 */
 	public function ipValid($ip)
 	{
-		# Get the IP Class.
-		require_once Utility::locateFile(MODULES.'IP'.DS.'IP.php');
-		# Create a new IP object.
-		$ip_obj=IP::getInstance();
-		return $ip_obj->ipValid($ip);
+		# Set valid to FALSE by default.
+		$valid=FALSE;
+		# Clean up the passed email address.
+		$ip=trim($ip);
+
+		# Check if the passed email address is NOT empty.
+		# It should be at least 7 characters (see http://stackoverflow.com/questions/22288483/whats-the-minimum-length-of-an-ip-address-in-string-representation)
+		# and no more than 45 characters (see http://stackoverflow.com/questions/1076714/max-length-for-client-ip-address).
+		$empty_email=$this->isEmpty($email, 7, 45);
+		if($empty_email===FALSE)
+		{
+			# Get the IP Class.
+			require_once Utility::locateFile(MODULES.'IP'.DS.'IP.php');
+			# Create a new IP object.
+			$ip_obj=IP::getInstance();
+			$valid=$ip_obj->ipValid($ip);
+		}
+		return $valid;
 	} #=== End -- ipValid
 
 	/*** End public methods ***/
 
 } # End Validator class.
-
-
-
-/**
- * EmailAddressValidator Class
- * http://code.google.com/p/php-email-address-validation/
- *
- * Released under New BSD license
- * http://www.opensource.org/licenses/bsd-license.php
- *
- * Sample Code
- * ----------------
- * $validator = new EmailAddressValidator;
- * if($validator->check_email_address('test@example.org'))
- * {
- * 	# Email address is technically valid
- * }
- */
-
-class EmailAddressValidator
-{
-
-	/**
-	 * check_email_address
-	 *
-	 * Check email address validity
-	 *
-	 * @param	strEmailAddress	Email address to be checked
-	 * @param	ping						Should it ping the domain name to see if it is an active domain?
-	 * @return								True if email is valid, FALSE if not
-	 * @access								public
-	 */
-	public function check_email_address($strEmailAddress, $ping=FALSE)
-	{
-		# If magic quotes is "on", email addresses with quote marks will
-		# fail validation because of added escape characters. Uncommenting
-		# the next four lines will allow for this issue.
-		//if(get_magic_quotes_gpc())
-		//{
-		//    $strEmailAddress = stripslashes($strEmailAddress);
-		//}
-
-		# Control characters are not allowed
-		if(preg_match('/[\x00-\x1F\x7F-\xFF]/', $strEmailAddress))
-		{
-			return FALSE;
-		}
-
-		# Check email length - min 3 (a@a), max 256
-		if(!$this->check_text_length($strEmailAddress, 3, 256))
-		{
-			return FALSE;
-		}
-
-		# Split it into sections using last instance of "@"
-		$intAtSymbol = strrpos($strEmailAddress, '@');
-		if($intAtSymbol===FALSE)
-		{
-			# No "@" symbol in email.
-			return FALSE;
-		}
-		$arrEmailAddress[0] = substr($strEmailAddress, 0, $intAtSymbol);
-		$arrEmailAddress[1] = substr($strEmailAddress, $intAtSymbol + 1);
-
-		/*
-		 Count the "@" symbols. Only one is allowed, except where
-		 contained in quote marks in the local part. Quickest way to
-		 check this is to remove anything in quotes. We also remove
-		 characters escaped with backslash, and the backslash
-		 character.
-		 */
-		$arrTempAddress[0] = preg_replace('/\./'
-			,''
-			,$arrEmailAddress[0]);
-		$arrTempAddress[0] = preg_replace('/"[^"]+"/'
-			,''
-			,$arrTempAddress[0]);
-		$arrTempAddress[1] = $arrEmailAddress[1];
-		$strTempAddress = $arrTempAddress[0] . $arrTempAddress[1];
-
-		# Then check - should be no "@" symbols.
-		if(strrpos($strTempAddress, '@')!==FALSE)
-		{
-			# "@" symbol found
-			return FALSE;
-		}
-
-		# Check local portion
-		if(!$this->check_local_portion($arrEmailAddress[0]))
-		{
-			return FALSE;
-		}
-
-		# Check domain portion
-		if(!$this->check_domain_portion($arrEmailAddress[1], $ping))
-		{
-			return FALSE;
-		}
-
-		# If we're still here, all checks above passed. Email is valid.
-		return TRUE;
-	}
-
-	/**
-	 * check_local_portion
-	 *
-	 * Checks email section before "@" symbol for validity
-	 *
-	 * @param	strLocalPortion     Text to be checked
-	 * @return	True if local portion is valid, FALSE if not
-	 * @access	protected
-	 */
-	protected function check_local_portion($strLocalPortion)
-	{
-		# Local portion can only be from 1 to 64 characters, inclusive.
-		# Please note that servers are encouraged to accept longer local
-		# parts than 64 characters.
-		if(!$this->check_text_length($strLocalPortion, 1, 64))
-		{
-			return FALSE;
-		}
-		# Local portion must be:
-		# 1) a dot-atom (strings separated by periods)
-		# 2) a quoted string
-		# 3) an obsolete format string (combination of the above)
-		$arrLocalPortion=explode('.', $strLocalPortion);
-		for($i=0, $max=sizeof($arrLocalPortion); $i<$max; $i++)
-		{
-			if(!preg_match('.^('.
-				'([A-Za-z0-9!#$%&\'*+/=?^_`{|}~-]'.
-				'[A-Za-z0-9!#$%&\'*+/=?^_`{|}~-]{0,63})'.
-				'|'.
-				'("[^\\\"]{0,62}")'.
-				')$.',
-				$arrLocalPortion[$i]))
-			{
-				return FALSE;
-			}
-		}
-		return TRUE;
-	}
-
-	/**
-	 * check_domain_portion
-	 *
-	 * Checks email section after "@" symbol for validity
-	 *
-	 * @param	strDomainPortion     Text to be checked
-	 * @return	True if domain portion is valid, FALSE if not
-	 * @access	protected
-	 */
-	protected function check_domain_portion($strDomainPortion, $ping=FALSE)
-	{
-		# Total domain can only be from 1 to 255 characters, inclusive
-		if(!$this->check_text_length($strDomainPortion, 1, 255))
-		{
-			return FALSE;
-		}
-		# Check if domain is IP, possibly enclosed in square brackets.
-		if(preg_match('/^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])'
-			.'(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$/'
-			,$strDomainPortion) ||
-			preg_match('/^\[(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])'
-			.'(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}\]$/'
-			,$strDomainPortion))
-		{
-			return TRUE;
-		}
-		else
-		{
-			$arrDomainPortion=explode('.', $strDomainPortion);
-			if(sizeof($arrDomainPortion)<2)
-			{
-				return FALSE; # Not enough parts to domain
-			}
-			for($i=0, $max=sizeof($arrDomainPortion); $i<$max; $i++)
-			{
-				# Each portion must be between 1 and 63 characters, inclusive
-				if(!$this->check_text_length($arrDomainPortion[$i], 1, 63))
-				{
-					return FALSE;
-				}
-				if (!preg_match('/^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|'
-					.'([A-Za-z0-9]+))$/', $arrDomainPortion[$i]))
-				{
-					return FALSE;
-				}
-				if($i==$max-1)
-				{ # TLD cannot be only numbers
-					if(strlen(preg_replace('/[0-9]/', '', $arrDomainPortion[$i])) <= 0)
-					{
-						return FALSE;
-					}
-				}
-			}
-		}
-		if($ping!==FALSE)
-		{
-			# Check if the email host is a valid host.
-			if(checkdnsrr($strDomainPortion)===FALSE)
-			{
-				return FALSE;
-			}
-		}
-		return TRUE;
-	}
-
-	/**
-	 * check_text_length
-	 *
-	 * Check given text length is between defined bounds
-	 *
-	 * @param	strText     Text to be checked
-	 * @param	intMinimum  Minimum acceptable length
-	 * @param	intMaximum  Maximum acceptable length
-	 * @return	True if string is within bounds (inclusive), FALSE if not
-	 * @access	protected
-	 */
-	protected function check_text_length($strText, $intMinimum, $intMaximum)
-	{
-		# Minimum and maximum are both inclusive
-		$intTextLength = strlen($strText);
-		if(($intTextLength<$intMinimum) || ($intTextLength>$intMaximum))
-		{
-			return FALSE;
-		}
-		else
-		{
-			return TRUE;
-		}
-	}
-
-} # End EmailAddressValidator
