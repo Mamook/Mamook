324d5bc1a342c3e72b1d7ec02368ca7dbfdb45db
Michael Delle <dravenlon@gmail.com>
2016-06-05 18:28:57 -0400

    Let's make our own PDF class that extends the FPDF library

---
+++ framework/application/modules/PDF/PDF.php
@@ -0,0 +1,33 @@
+<?php /* framework/application/modules/PDF/PDF.php */
+
+# Make sure the script is not accessed directly.
+if(!defined('BASE_PATH')) exit('No direct script access allowed');
+
+# Get the FPDF Class.
+require_once Utility::locateFile(MODULES.'Vendor'.DS.'FPDF'.DS.'fpdf.php');
+
+/**
+ * PDF
+ *
+ * The PDF class is used to generate PDF files.
+ *
+ */
+class PDF extends FPDF
+{
+	/**
+	 * Footer()
+	 *
+	 * PDF page footer.
+	 *
+	 * @access protected
+	 */
+	protected function Footer()
+	{
+		# Position at 1.5 cm from bottom
+		$this->SetY(-15);
+		# Arial italic 8
+		$this->SetFont('Arial','I',8);
+		# Page number
+		$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
+} #=== End PDF class.
