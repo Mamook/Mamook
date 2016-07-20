7b0c616bf8654bbeb10f1acc8135781d5db0d617
Jon Ryser <contact@jonryser.com>
2016-07-20 07:38:35 -0700

    Cleaned up media JS.

--- framework/application/js/media.php
+++ framework/application/js/media.php
@@ -1,16 +1,19 @@
 <?php /* application/js/media.php */
 
-$js='$(".arrow-prev").addClass("disabled");'.
-	'var c=$(".feed-video > li").size(),'.
-	'd=$(".feed-audio > li").size();'.
-	'if(c<4 && d<7)'.
-		'$(".arrow-next").addClass("disabled");';
+$js='(function(a){a(function(){a(".arrow-prev").addClass("disabled");var b=a(".feed-video > li").size(),c=a(".feed-audio > li").size();4>b&&7>c&&a(".arrow-next").addClass("disabled")})})(jQuery);';
+
 
 /* Big
-	// Initially add the "disabled" class to the "previous" button.
-	$(".arrow-prev").addClass("disabled");
-	var numberOfVideos=$(".video-feed > li").size();
-	var numberOfAudioFiles=$(".audio-feed > li").size();
-	if(numberOfVideos<4 && numberOfAudioFiles<7)
-		$(".arrow-next").addClass("disabled");
-*/
+	// Wrap the script to protect the global namespace.
+	(function($){
+			// Wait for document ready.
+			$(function(){
+					// Initially add the "disabled" class to the "previous" button.
+					$(".arrow-prev").addClass("disabled");
+					var numberOfVideos = $(".feed-video > li").size();
+					var numberOfAudioFiles = $(".feed-audio > li").size();
+					if(numberOfVideos<4 && numberOfAudioFiles<7) {
+							$(".arrow-next").addClass("disabled");
+					}
+			})
+	})(jQuery);*/
