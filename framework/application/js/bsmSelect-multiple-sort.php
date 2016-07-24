d271956e631899942c720cb28a37b3320f200e23
Jon Ryser <contact@jonryser.com>
2016-07-23 22:39:07 -0700

    Cleaned up bsmSelect multiple JS. Protected global namespace. Made it wait for document ready.

--- framework/application/js/bsmSelect-multiple-sort.php
+++ framework/application/js/bsmSelect-multiple-sort.php
@@ -1,13 +1,24 @@
 <?php /* application/js/bsmSelect-multiple-sort.php */
 
-$js='$(\'select[multiple]\').bsmSelect({'.
-		'addItemTarget: \'top\','.
-		'hideWhenAdded: true,'.
-		'removeLabel: \'X\','.
-		'animate: true,'.
-		'highlight: true,'.
-		'plugins: ['.
-			'$.bsmSelect.plugins.sortable({ axis : \'y\', opacity : 0.5 }, { listSortableClass : \'bsmListSortableCustom\' }),'.
-			'$.bsmSelect.plugins.compatibility()'.
-		']'.
-	'});';
+$js='(function(a){a(function(){a("select[multiple]").bsmSelect({addItemTarget:"top",hideWhenAdded:!0,removeLabel:"X",animate:!0,highlight:!0,plugins:[a.bsmSelect.plugins.sortable({axis:"y",opacity:0.5},{listSortableClass:"bsmListSortableCustom"}),a.bsmSelect.plugins.compatibility()]})})})(jQuery);';
+
+
+/* Big version
+// Wrap the script to protect the global namespace.
+(function ($) {
+	// Wait for document ready.
+	$(function () {
+		$("select[multiple]").bsmSelect({
+			addItemTarget: "top",
+			hideWhenAdded: true,
+			removeLabel: "X",
+			animate: true,
+			highlight: true,
+			plugins: [
+				$.bsmSelect.plugins.sortable({ axis: "y", opacity: 0.5 }, { listSortableClass: "bsmListSortableCustom" }),
+				$.bsmSelect.plugins.compatibility()
+			]
+		});
+	});
+})(jQuery);
+*/
--- framework/application/js/bsmSelect-multiple.php
+++ framework/application/js/bsmSelect-multiple.php
@@ -1,21 +1,35 @@
 <?php /* application/js/bsmSelect-multiple.php */
-$js='$("select[multiple]").bsmSelect({addItemTarget:"original",hideWhenAdded:!1,removeLabel:"X",animate:!0,plugins:[$.bsmSelect.plugins.compatibility()],containerClass:"bsm",selectClass:"select",listClass:"bsm-list",listItemClass:"bsm-item",listItemLabelClass:"bsm-label",removeClass:"remove"});$(".remove").attr({title:"Remove This"});$("select[multiple]").change(function(){$(".remove").attr({title:"Remove This"})});';
+$js='(function(a){a(function(){a("select[multiple]").bsmSelect({addItemTarget:"original",hideWhenAdded:!1,removeLabel:"X",animate:!0,plugins:[a.bsmSelect.plugins.compatibility()],containerClass:"bsm",selectClass:"select",listClass:"bsm-list",listItemClass:"bsm-item",listItemLabelClass:"bsm-label",removeClass:"remove"});a(".remove").attr({title:"Remove This"});a("select[multiple]").change(function(){a(".remove").attr({title:"Remove This"})})})})(jQuery);';
 
-# Big
-/* $js='$("select[multiple]").bsmSelect({'.
-		'addItemTarget:"original",'.
-		'hideWhenAdded:false,'.
-		'removeLabel:"X",'.
-		'animate:true,'.
-		'plugins:[$.bsmSelect.plugins.compatibility()],'.
-		'containerClass:"bsm",'.						// Class for container that wraps this widget
-		'selectClass:"select",'.						// Class for the <select>
-		'listClass:"bsm-list",'.						// Class for the list ($ol)
-		'listItemClass:"bsm-item",'.				// Class for the <li> list items
-		'listItemLabelClass:"bsm-label",'.	// Class for the label text that appears in list items
-		'removeClass:"remove"'.							// Class given to the "remove" link
-	'});'.
-	'$(".remove").attr({title:"Remove This"});'.
-	'$("select[multiple]").change(function(){'.
-		'$(".remove").attr({title:"Remove This"})'.
-	'});'; */
+
+/* Big version
+// Wrap the script to protect the global namespace.
+(function ($) {
+	// Wait for document ready.
+	$(function () {
+		$("select[multiple]").bsmSelect({
+			addItemTarget: "original",
+			hideWhenAdded: false,
+			removeLabel: "X",
+			animate: true,
+			plugins: [$.bsmSelect.plugins.compatibility()],
+			// Class for container that wraps this widget
+			containerClass: "bsm",
+			// Class for the <select>
+			selectClass: "select",
+			// Class for the list (ol)
+			listClass: "bsm-list",
+			// Class for the <li> list items
+			listItemClass: "bsm-item",
+			// Class for the label text that appears in list items
+			listItemLabelClass: "bsm-label",
+			// Class given to the "remove" link
+			removeClass: "remove"
+		});
+		$(".remove").attr({ title:"Remove This" });
+		$("select[multiple]").change(function () {
+			$(".remove").attr({ title:"Remove This" })
+		});
+	});
+})(jQuery);
+*/
