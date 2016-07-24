5679e44b8a91ed402cfae0dd852eed4109b9b1da
Jon Ryser <contact@jonryser.com>
2016-07-24 10:34:54 -0700

    Cleaned up removeCurrentItem JS. Protected global namespace.

--- framework/application/js/removeCurrentItem.php
+++ framework/application/js/removeCurrentItem.php
@@ -1,20 +1,24 @@
 <?php /* application/js/removeCurrentItem.php */
 
 # Minified version.
-$js='$(function(){$(".file-current .remove").click(function(){var a=$(this).closest(".file-current"),b=$(this).prev("input").clone();b.attr({name:b.attr("name")+"_remove"}).appendTo(a.closest("li"));a.remove();return!1})});';
+$js='(function(a){a(function(){a(".file-current .remove").click(function(){var b=a(this).closest(".file-current"),c=a(this).prev("input").clone();c.attr({name:c.attr("name")+"_remove"}).appendTo(b.closest("li"));b.remove();return!1})})})(jQuery);';
 
-# Long version.
-/*$js='// Wrap it all in the jQuery(document).ready method to ensure the internal variables and functions are not invasive to the page and so that this will execute after the document is loaded.
-	$(function(){
+
+/* Big version
+// Wrap the script to protect the global namespace.
+(function ($) {
+	// Wait for document ready.
+	$(function () {
 		// Create a local variable to hold the "remove" button. When it is clicked, remove the parent element with the class "file-current".
-		var removeButton = $(".file-current .remove")
-			.click(function(){
-				var currentFileContainer = $(this).closest(".file-current");
-				// Clone the hidden input.
-				var hidden = $(this).prev("input").clone();
-				hidden.attr({name:hidden.attr("name") + "_remove"})
-					.appendTo(currentFileContainer.closest("li"));
-				currentFileContainer.remove();
-				return false;
-			});
-	});';*/
+		$(".file-current .remove").click(function(){
+			var currentFileContainer = $(this).closest(".file-current");
+			// Clone the hidden input.
+			var hidden = $(this).prev("input").clone();
+			hidden.attr({name:hidden.attr("name") + "_remove"})
+				.appendTo(currentFileContainer.closest("li"));
+			currentFileContainer.remove();
+			return false;
+		});
+	});
+})(jQuery);
+*/
