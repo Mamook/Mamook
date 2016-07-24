0d2a026b01b7fb4ce6a85717b9c0caeb1326bc29
Jon Ryser <contact@jonryser.com>
2016-07-24 09:35:30 -0700

    Cleaned up uniform-select JS. Protected global namespace.

--- framework/application/js/uniform-select.php
+++ framework/application/js/uniform-select.php
@@ -1,3 +1,15 @@
 <?php /* application/js/uniform-select.php */
 
-$js='$(function(){$(\'select.select\').not(\'select[multiple]\').uniform()});';
+$js='(function(a){a(function(){a("select.select").not("select[multiple]").uniform()})})(jQuery);';
+
+
+/* Big version
+// Wrap the script to protect the global namespace.
+(function ($) {
+	// Wait for document ready.
+	$(function () {
+		$("select.select").not("select[multiple]")
+			.uniform();
+	});
+})(jQuery);
+*/
